6f2e6b3ba5776faf543ab6e19b6a7030d9a2f895
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-23 01:57:49 +0200

    Finished the Hello Triangle example program

--- src/main.c
+++ src/main.c
@@ -1,99 +1,179 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-struct DynamicArray {
-    int* start;
-    int* capacity;
-    int* end;
-};
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <mach/vm_region.h>
 
-/*
- * This only returns 0 on an error case, otherwise return value is 1 on input 0 and 1, and otherwise the next 2^n such that 2^{n - 1} < x <= 2^n
- * We guarantee that returnValue >= inputValue, with equality only if n = 2^k for some k >= 1
- * There are no overflow checks, so be careful that n is not too large.
- */
-static size_t next_power_of_2(const size_t n) {
-    if(n == 0) return 0;
-    if ((n & (n - 1)) == 0) return n;
+GLFWwindow* g_window;
 
-    size_t cmp = 1;
-    while(cmp < n) cmp *= 2;
-    return cmp;
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+
+void framebufferSizeCallback(
+    GLFWwindow* window __attribute__((unused)),
+    const int width,
+    const int height)
+{
+    glViewport(0, 0, width, height);
 }
 
-/*
- * It is the users responsibility to free arr.start
- */
-struct DynamicArray DynamicArray_init(const size_t num_elements) {
-    struct DynamicArray arr;
-
-    arr.start = (int*)malloc(num_elements * sizeof(int));
-    if(arr.start == NULL) {
-        fprintf(stderr, "Memory Allocation Failed!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    arr.capacity = arr.start + next_power_of_2(num_elements);
-    arr.end = arr.start + num_elements;
-
-    // Initialize array values to 0
-    for(int* addr = arr.start; addr < arr.capacity; addr++) *addr = 0;
-
-    return arr;
+void processInput() {
+    if(glfwGetKey(g_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(g_window, true);
 }
-
-int DynamicArray_get(const struct DynamicArray* arr, const size_t idx) {
-    if(arr->start + idx >= arr->end) {
-        fprintf(stderr, "Index out of bounds: %zu\n", idx);
-        exit(EXIT_FAILURE);
-    }
-    return *(arr->start + idx);
-}
-
-/*
- * While this doesn't modify the struct pointers, it does, of course, change the underlying memory so it's not const in that way.
- */
-void DynamicArray_set(const struct DynamicArray* arr, const size_t idx, const int value) {
-    if(arr->start + idx >= arr->end) {
-        fprintf(stderr, "Index out of bounds: %zu\n", idx);
-        exit(EXIT_FAILURE);
-    }
-    *(arr->start + idx) = value;
-}
-
-void DynamicArray_push_back(struct DynamicArray* arr, const int value) {
-    // Resize with factor 2
-    if(arr->end == arr->capacity) {
-        const size_t current_size = arr->end - arr->start;
-        const size_t new_capacity = (arr->capacity - arr->start) * 2;
-
-        int* new_start = realloc(arr->start, new_capacity * sizeof(int));
-        if(new_start == NULL) {
-            fprintf(stderr, "Memory reallocation failed!\n");
-            exit(EXIT_FAILURE);
-        }
-
-        arr->start = new_start;
-        arr->end = arr->start + current_size;
-        arr->capacity = arr->start + new_capacity;
-    }
-    *arr->end = value;
-    arr->end += 1;
-}
-
 
 int main() {
-    const size_t num_elements = 7;
-    struct DynamicArray arr = DynamicArray_init(num_elements);
+    printf("Initializing OpenGL.\n");
+    if(!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW!\n");
+        return EXIT_FAILURE;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    for(size_t i = 0; i < num_elements; i++) printf("arr.start + %zu : %p -> %d\n", i, arr.start + i, *(arr.start + i));
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    printf("End pointer: %p\n", arr.end);
-    printf("Capacity pointer: %p\n", arr.capacity);
 
-    // Free allocated memory
-    free(arr.start);
-    arr.start = NULL;
+    g_window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (g_window == NULL) {
+        fprintf(stderr, "Failed to create GLFW window");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(g_window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD");
+        return EXIT_FAILURE;
+    }
+
+    int frame_buffer_width, frame_buffer_height;
+    // this is in general different from the window size, for example on Apple retina displays which downscale your window.
+    glfwGetFramebufferSize(g_window, &frame_buffer_width, &frame_buffer_height);
+    glViewport(0, 0, frame_buffer_width, frame_buffer_height);
+
+    glfwSetFramebufferSizeCallback(g_window, framebufferSizeCallback);
+
+    const float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f,  // top left
+        0.0f,  0.5f, 0.0f   // top center
+   };
+    const unsigned int indices[] = {
+        3, 2, 0,
+        2, 1, 0
+    };
+
+    unsigned int VAO, VBO, EBO;
+
+    // Generate and bind the VAO
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Generate and bind the VBO for the vertex data
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Generate and bind the EBO for the element (index) data
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Set the vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind the VAO (optionally)
+    glBindVertexArray(0);
+
+    printf("Trying to compile and link shaders into a shader program.");
+    int success_flag;
+    const unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success_flag);
+    if(!success_flag) {
+        char infoLog[512];
+        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+        fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+        fprintf(stderr, infoLog);
+        return EXIT_FAILURE;
+    }
+
+    const unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success_flag);
+    if(!success_flag) {
+        char info_log[512];
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+        fprintf(stderr, info_log);
+        return EXIT_FAILURE;
+    }
+
+    printf("Finished compiling shaders, creating shader program now.");
+    const unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success_flag);
+    if(!success_flag) {
+        char info_log[512];
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        fprintf(stderr, "ERROR::SHADER::PROGRAM::LINKING_FAILED\n");
+        fprintf(stderr, info_log);
+    }
+
+    printf("Shaders successfully compiled!\n");
+
+    const GLubyte* version = glGetString(GL_VERSION);
+    printf("Machine has OpenGL version: '%s'\n", (const char*)version);
+    printf("Minimal required OpenGL version is 3.3.0.\n");
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    printf("\n");
+    printf("Finished initializing OpenGL.\n");
+    printf("Starting mainloopL.\n");
+    while(!glfwWindowShouldClose(g_window)) {
+        processInput(g_window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+
+        glfwSwapBuffers(g_window);
+        glfwPollEvents();
+    }
+    printf("Finished running mainloop.\n");
+
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
